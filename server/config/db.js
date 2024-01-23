924e0a658ee818da1734cb7589e5271d9e4b26d4
Mieshkov Andrii <a.mieshkov.ip15@kpi.ua>
2024-01-23 18:01:41 +0100

    fix

--- server/config/db.js
+++ server/config/db.js
@@ -5,9 +5,10 @@ const username = process.env.DATABASE_USERNAME;
 const password = process.env.DATABASE_PASSWORD;
 const host = process.env.DATABASE_HOST;
 const port = process.env.DATABASE_PORT;
+const name = process.env.DATABASE_NAME;
 
 const sequelize = new Sequelize(
-    'SuperCoffee',
+    name,
     username,
     password,
     {
